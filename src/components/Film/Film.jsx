de23d92139bef63c6d6a2c1cd111e49cf9aff042
Swiftrade <swiftradeera@gmail.com>
2022-02-03 13:34:10 +0300

    Fix with adaptability and description

--- src/components/Film/Film.jsx
+++ src/components/Film/Film.jsx
@@ -2,38 +2,75 @@ import React from 'react';
 import { Card, Tag, Col, Rate } from 'antd';
 import { format, parseISO } from 'date-fns';
 import PropTypes from 'prop-types';
+import { CW } from '../../services/ClientWidth';
 import Vote from '../Vote';
 import failureImg from '../../assets/img/imgnotavailable.png';
 
-const Film = ({ title, poster, description, date, vote }) => (
-  <Col>
-    <Card className="film">
-      <div className="film__card">
-        <div className="film__poster">
-          <img
-            className="film__poster-img"
-            src={poster ? `https://image.tmdb.org/t/p/w500${poster}` : failureImg}
-            alt="poster"
-            style={!poster ? { objectFit: 'contain' } : null}
-          />
-        </div>
-        <div className="film__info">
-          <div className="film__info--header">
-            <h5 className="film__title">{title}</h5>
-            <Vote vote={vote} />
+const Film = ({ title, poster, description, date, vote }) => {
+  if (CW > 580) {
+    return (
+      <Col>
+        <Card className="film">
+          <div className="film__card">
+            <div className="film__poster">
+              <img
+                className="film__poster-img"
+                src={poster ? `https://image.tmdb.org/t/p/w500${poster}` : failureImg}
+                alt="poster"
+                style={!poster ? { objectFit: 'contain' } : null}
+              />
+            </div>
+            <div className="film__info">
+              <div className="film__info--header">
+                <h5 className="film__title">{title}</h5>
+                <Vote vote={vote} />
+              </div>
+              <h6 className="film__date">{date ? format(parseISO(date), 'd LLLL, yyyy') : 'Unknown'}</h6>
+              <div className="film__tags">
+                <Tag>Action</Tag>
+                <Tag>Drama</Tag>
+              </div>
+              <p className="film__description">{description}</p>
+              <Rate count={10} className="film__rate" />
+            </div>
           </div>
-          <h6 className="film__date">{date ? format(parseISO(date), 'd LLLL, yyyy') : 'Unknown'}</h6>
-          <div className="film__tags">
-            <Tag>Action</Tag>
-            <Tag>Drama</Tag>
+        </Card>
+      </Col>
+    );
+  }
+  return (
+    <Col>
+      <Card className="film">
+        <div className="film__card">
+          <div className="film__header">
+            <div className="film__poster">
+              <img
+                className="film__poster-img"
+                src={poster ? `https://image.tmdb.org/t/p/w500${poster}` : failureImg}
+                alt="poster"
+                style={!poster ? { objectFit: 'contain' } : null}
+              />
+            </div>
+            <div className="film__info">
+              <div className="film__info--header">
+                <h5 className="film__title">{title}</h5>
+                <Vote vote={vote} />
+              </div>
+              <h6 className="film__date">{date ? format(parseISO(date), 'd LLLL, yyyy') : 'Unknown'}</h6>
+              <div className="film__tags">
+                <Tag>Action</Tag>
+                <Tag>Drama</Tag>
+              </div>
+            </div>
           </div>
+
           <p className="film__description">{description}</p>
           <Rate count={10} className="film__rate" />
         </div>
-      </div>
-    </Card>
-  </Col>
-);
+      </Card>
+    </Col>
+  );
+};
 Film.defaultProps = {
   poster: null,
   date: null,
---
+++ src/services/ClientWidth.js
@@ -0,0 +1,1 @@
+export const CW = window.screen.availWidth;
