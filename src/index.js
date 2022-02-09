8b676c96a24ce1c5143a543e0378a925d5fe1c65
Swiftrade <swiftradeera@gmail.com>
2022-02-09 10:13:53 +0300

    last push

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 import 'antd/dist/antd.min.css';
-import './styles/style.scss';
+import './style.scss';
 
 ReactDOM.render(<App />, document.getElementById('root'));
